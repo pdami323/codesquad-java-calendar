5e5ce7fac0b0edd746b9e97a9b7065d9a4d62100
Dami Park <ekal0323@gmail.com>
2022-10-21 15:17:18 +0900

    Fix calendar main()

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -4,33 +4,12 @@ public class Calendar {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, Calendar");
-		System.out.println(" 일  월 화  수 목  금 토");
+		System.out.println(" 일  월  화  수 목  금  토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-//		char[] day = {'일','월', '화', '수', '목', '금', '토'};
-//		for(int i=0;i<7;i++) {
-//			System.out.print(day[i] + "  ");
-//		}
-//		System.out.println();
-//		for(int i=0;i<=7;i++) {
-//			System.out.print("---");
-//		}
-//		System.out.println();
-//		int n_day = 1;
-//		while(n_day <29) {
-//			if(n_day < 10) {
-//				System.out.print(" " + n_day + " ");
-//			}else {
-//				System.out.print(n_day + " ");
-//			}
-//			if(n_day % 7 ==0) {
-//				System.out.println();
-//			}
-//			n_day++;
-//		}
 	}
 
 }
