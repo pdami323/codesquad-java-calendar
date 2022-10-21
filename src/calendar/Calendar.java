adbf0b92ad824809d414a81022fb1644af29d7cc
Dami Park <ekal0323@gmail.com>
2022-10-21 15:55:09 +0900

    Fix find max days of the month

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -15,8 +15,10 @@ public class Calendar {
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		int[] day = new int[12];
 		for(int i=1;i<=12;i++) {
-			if(i%2 == 0&&i!= 8) {
+			if(i%2 == 0&&i!= 8&&i!=2) {
 				day[i-1] = 30;
+			}else if(i==2) {
+				day[i-1] = 28;
 			}else {
 				day[i-1] = 31;
 			}
@@ -24,7 +26,7 @@ public class Calendar {
 		System.out.println("달을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
 		int month = scanner.nextInt();
-		System.out.printf("%d월은 %d일까지 있습니다.", month, day[month-1]);
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, day[month-1]);
 		scanner.close();
 	}
 
